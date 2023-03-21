d463cf79e9f444f3225faea90caf671c22da6535
martijnvdk26 <martijnvanderkooi@gmail.com>
2023-03-21 11:01:56 +0100

    Afronden op 2 decimalen

--- WPF_Calculator/WPF_Calculator/ExchangeWindow.xaml.cs
+++ WPF_Calculator/WPF_Calculator/ExchangeWindow.xaml.cs
@@ -47,13 +47,13 @@ namespace WPF_Calculator
 
         private void Convert_Click(object sender, RoutedEventArgs e) 
         {
-            double tot;
+            double tot;            
             double amount = double.Parse(AmountTB.Text);
 
             if (((ComboBoxItem)Cbox1.SelectedItem).Content.ToString() == "Euro" &&
                 ((ComboBoxItem)Cbox2.SelectedItem).Content.ToString() == "Dollar")
             {
-                tot = amount * 1.07;
+                tot = Math.Round(amount * 1.07, 2);
                 ExchangeTB.Text = "1 euro is " + tot.ToString() + " dollar";
 
             }
@@ -61,35 +61,35 @@ namespace WPF_Calculator
             else if (((ComboBoxItem)Cbox1.SelectedItem).Content.ToString() == "Euro" &&
                     ((ComboBoxItem)Cbox2.SelectedItem).Content.ToString() == "Pond")
             {
-                tot = amount * 0.89;
+                tot = Math.Round(amount * 0.89, 2);
                 ExchangeTB.Text = "1 euro is " + tot.ToString() + " pond";
             }
 
             else if (((ComboBoxItem)Cbox1.SelectedItem).Content.ToString() == "Dollar" &&
                     ((ComboBoxItem)Cbox2.SelectedItem).Content.ToString() == "Pond")
             {
-                tot = amount * 0.83;
+                tot = Math.Round(amount * 0.83, 2);
                 ExchangeTB.Text = "1 Dollar is " + tot.ToString() + " pond";
             }
 
             else if (((ComboBoxItem)Cbox1.SelectedItem).Content.ToString() == "Dollar" &&
                     ((ComboBoxItem)Cbox2.SelectedItem).Content.ToString() == "Euro")
             {
-                tot = amount * 0.93;
+                tot = Math.Round(amount * 0.93, 2);
                 ExchangeTB.Text = "1 Dollar is " + tot.ToString() + " euro";
             }
 
             else if (((ComboBoxItem)Cbox1.SelectedItem).Content.ToString() == "Pond" &&
                     ((ComboBoxItem)Cbox2.SelectedItem).Content.ToString() == "Euro")
             {
-                tot = amount * 1.13;
+                tot = Math.Round(amount * 1.13, 2);
                 ExchangeTB.Text = "1 pond is " + tot.ToString() + " euro";
             }
 
             else if (((ComboBoxItem)Cbox1.SelectedItem).Content.ToString() == "Pond" &&
                     ((ComboBoxItem)Cbox2.SelectedItem).Content.ToString() == "Dollar")
             {
-                tot = amount * 1.21;
+                tot = Math.Round(amount * 1.21, 2);
                 ExchangeTB.Text = "1 pond is " + tot.ToString() + " dollar";
             }
 
