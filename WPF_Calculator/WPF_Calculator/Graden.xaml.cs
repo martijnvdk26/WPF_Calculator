737b4a5856985320f1a1fe80561ebf1ac71b46b1
ChrisvdBos <chris.van.der.bos1@student.nhlstenden.com>
2023-06-01 13:30:43 +0200

    Graden
    titel.

--- WPF_Calculator/WPF_Calculator/Graden.xaml.cs
+++ WPF_Calculator/WPF_Calculator/Graden.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_Calculator.MetricImperialContent;
 
 namespace WPF_Calculator
 {
@@ -20,9 +21,110 @@ namespace WPF_Calculator
     /// </summary>
     public partial class Graden : UserControl
     {
-        public Graden()
+		private int Decimalen = 2;
+
+		public Graden()
         {
             InitializeComponent();
+		}
+
+        public void Omzetten()
+        {
+            try
+            {
+                double GradenInput = 0.00;
+                double RadialenInput = 0.00;
+				double GonInput = 0.00;
+
+                if (double.TryParse(GradenTextBox.Text, out GradenInput) && !double.TryParse(RadialenTextBox.Text, out RadialenInput) && !double.TryParse(GonTextBox.Text, out GonInput))
+                {
+					Bereken("Graden", GradenInput);
+                } 
+                else if (!double.TryParse(GradenTextBox.Text, out GradenInput) && double.TryParse(RadialenTextBox.Text, out RadialenInput) && !double.TryParse(GonTextBox.Text, out GonInput))
+                {
+					Bereken("Radialen", RadialenInput);
+				} 
+                else if (!double.TryParse(GradenTextBox.Text, out GradenInput) && !double.TryParse(RadialenTextBox.Text, out RadialenInput) && double.TryParse(GonTextBox.Text, out GonInput))
+                {
+					Bereken("Gon", GonInput);
+				} 
+                else if (!double.TryParse(GradenTextBox.Text, out GradenInput) || !double.TryParse(RadialenTextBox.Text, out RadialenInput) || !double.TryParse(GonTextBox.Text, out GonInput))
+                {
+					ErrorOutput.Text = "Foutieve invoer.";
+				} 
+                else
+                {
+					ErrorOutput.Text = "Voer A.U.B alleen 1 veld in. Click op 'Clear' om de velden leeg te maken.";
+				}
+			}
+            catch
+            {
+                ErrorOutput.Text = "Foutieve invoer.";
+            }
         }
-    }
+
+		private void Bereken(string type, double value)
+		{
+			switch (type)
+			{
+				case "Graden":
+					{
+						RadialenTextBox.Text = Math.Round((value * 0.01745329), Decimalen).ToString();
+						GonTextBox.Text = Math.Round((value * 1.1111), Decimalen).ToString();
+						ErrorOutput.Text = (100 / 90).ToString();
+						break;
+					}
+				case "Radialen":
+					{
+						GradenTextBox.Text = Math.Round((value * 57.2958), Decimalen).ToString();
+						GonTextBox.Text = Math.Round((value * 63.662), Decimalen).ToString();
+						break;
+					}
+				case "Gon":
+					{
+						GradenTextBox.Text = Math.Round((value / 1.1111), Decimalen).ToString();
+						RadialenTextBox.Text = Math.Round((value * 0.015708), Decimalen).ToString();
+						break;
+					}
+			}
+			ErrorOutput.Text = "";
+		}
+
+		private void ClearKnop_Click(object sender, RoutedEventArgs e)
+		{
+            GradenTextBox.Clear();
+            RadialenTextBox.Clear();
+            GonTextBox.Clear();
+		}
+
+		private void Omzetknop_Click(object sender, RoutedEventArgs e)
+		{
+            Omzetten();
+		}
+
+		private void DecimalenInput_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (ErrorOutput != null)
+			{
+				try
+				{
+					if (Convert.ToInt32(DecimalenInput.Text) > 15)
+					{
+						Decimalen = 15;
+						ErrorOutput.Text = "Te Grote Decimalen Invoer. Het Maximale 15 wordt gebruikt.";
+					}
+					else
+					{
+						Decimalen = Convert.ToInt32(DecimalenInput.Text);
+						ErrorOutput.Text = "";
+					}
+				}
+				catch
+				{
+					Decimalen = 2;
+					ErrorOutput.Text = "Foutieve Decimalen Invoer. Standaard 2 wordt gebruikt.";
+				}
+			}
+		}
+	}
 }
